d0edff938d6dd712e7efdecd570e21a54ec0153d
atthapornh <autthapornh62@nu.ac.th>
2021-08-23 14:26:33 +0700

    Steak 62316062

--- client/src/components/ShowUser.vue
+++ client/src/components/ShowUser.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <h1>Show User</h1>
+    <h1>ข้อมูล Steak</h1>
     <p>id: {{ userId }}</p>
-    <p>ชื่อ - นามสกุล {{ user.name }} - {{ user.lastname }}</p>
-    <p>email: {{ user.email }}</p>
-    <p>password: {{ user.password }}</p>
+    <p>ระดับความสุก {{ user.name }} - {{ user.lastname }}</p>
+    <p>ช่องทางการติดต่อ email: {{ user.email }}</p>
+    <p>ราคา: {{ user.password }}</p>
   </div>
 </template>
 <script>
